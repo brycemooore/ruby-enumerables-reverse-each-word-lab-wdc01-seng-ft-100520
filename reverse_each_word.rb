fc6ae4d65e70be7a4ffb3170ff2002a71206efef
Bryce Moore <brycemooore@gmail.com>
2020-09-23 21:46:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,9 @@ def reverse_each_word(input)
   word_array.each {|word|
   split_string = word.split("")
   reversed_string = ""
-  
+  word.size.times {
+    
+  }
   }
   output = word_array.join(" ")
   output
